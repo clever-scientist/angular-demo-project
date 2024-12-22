841a0c5fb8ad61f65b2b9d0a4ededfd1daa48837
Ayoub Bouziane <a.bouziane@indatacore.com>
2024-12-22 02:25:55 +0100

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,4 +10,9 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'my-demo-project';
+
+  SayHello() {
+    console.log('Hello, my-demo-project');
+
+  }
 }
